d254f86c5ca5e7ed9b6b55eaec726970f8448246
Matteo Zanella <matteo.zanella-2@studenti.unitn.it>
2020-06-19 16:34:35 +0200

    Added graph analysis

---
+++ analysis.py
@@ -0,0 +1,30 @@
+import math
+import numpy as np
+import graph_tool.all as gt
+
+
+def comm_membership(c: gt.VertexPropertyMap):
+    graph = c.get_graph()
+    o_min = math.inf
+    o_max = 0
+    o_mean = 0
+    for v in graph.get_vertices():
+        o_mean += len(c[v]) / graph.num_vertices()
+        o_min = o_min if o_min < len(c[v]) else len(c[v])
+        o_max = o_max if o_max > len(c[v]) else len(c[v])
+    return o_min, o_max, o_mean
+
+def comm_size(c: gt.VertexPropertyMap, num_comm: int):
+    graph = c.get_graph()
+    c_min = math.inf
+    c_max = 0
+    c_mean = 0
+    sizes = np.zeros(num_comm, dtype='int')
+    for v in graph.get_vertices():
+        for label in c[v]:
+            sizes[label] += 1
+    for c_size in sizes:
+        c_min = c_min if c_min < c_size else c_size
+        c_max = c_max if c_max > c_size else c_size
+        c_mean += c_size / num_comm
+    return c_min, c_max, c_mean
